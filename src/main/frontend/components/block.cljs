2e15a016f8864a0fd8641e477b5e6b381d56b4db
Tienson Qin <tiensonqin@gmail.com>
2022-03-30 14:34:17 +0800

    fix: lazy loading ids

--- src/main/frontend/components/block.cljs
+++ src/main/frontend/components/block.cljs
@@ -2903,7 +2903,7 @@
                                (load-more-blocks! config flat-blocks)))
             has-more? (and
                        (> (count flat-blocks) model/initial-blocks-length)
-                       (some? (model/get-next-open-block (last flat-blocks) db-id)))
+                       (some? (model/get-next-open-block (db/get-conn) (last flat-blocks) db-id)))
             dom-id (str "lazy-blocks-" (::id state))]
         [:div {:id dom-id}
          (ui/infinite-list
